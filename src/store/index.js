d285f7b8f0186dd5766a71697797e635bc33f359
yixiancc <39053906+yixiancc@users.noreply.github.com>
2024-04-07 14:32:54 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -20,12 +20,12 @@ export const useCommonCache = defineStore({
     // 定义 actions
     actions: {
         // 判断用户是否能够访问，利用router.hasRoute，因为不能访问的已经remove了
-        // judgeAuthority(to) {
-        //     return new Promise((resolve, reject) => {
-        //         const router = useRouter()
-        //         resolve(router.hasRoute(to.name))
-        //     })
-        // }
+        judgeAuthority(to) {
+            return new Promise((resolve, reject) => {
+                const router = useRouter()
+                resolve(router.hasRoute(to.name))
+            })
+        }
     }
 })
 
